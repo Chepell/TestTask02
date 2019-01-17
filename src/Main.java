891dfd590171263abb50a0a147058e07645a139f
Chepell <av.voytenko@gmail.com>
2019-01-17 20:01:16 +0300

    Реализован метод фильрцации листьев и вывода правильного дерева в консоль.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,20 @@
 import jdbc.DatabaseHandlerAdjacencyList;
 
-import java.sql.SQLException;
-
 /**
  * Artem Voytenko
  * 15.01.2019
  */
 
 public class Main {
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+	public static void main(String[] args) {
 
 		DatabaseHandlerAdjacencyList handlerBase = new DatabaseHandlerAdjacencyList();
-		handlerBase.getFullTree();
 
-		System.out.println("\n**************\n");
-		handlerBase.getFilterLeafs("d");
+		handlerBase.getFullTree();
 
 		System.out.println("\n**************\n");
 
 		handlerBase.getFilterTree("d");
-		System.out.println("\n**************\n");
 
 		handlerBase.closeConnectionToBD();
 	}
--- src/jdbc/DatabaseHandlerAdjacencyList.java
+++ src/jdbc/DatabaseHandlerAdjacencyList.java
@@ -17,77 +17,36 @@ public class DatabaseHandlerAdjacencyList {
 	private String dbPass = "sbertest";
 	private String dbSchemaName = "sber_tabel";
 	private static final String TABLE = "folders";
+	private static final String TMP_TABLE = TABLE + "_tmp";
 	private static final String ID = "id";
 	private static final String PARENT_ID = "parent_id";
 	private static final String NAME = "name";
 	//
 	private Connection dbConnection;
 
-	// метод возвращает коннекшн к БД
-	public Connection getDbConnection() throws SQLException {
-		// собираю строку коннешена
+	/**
+	 * метод создания коннекшена к БД
+	 *
+	 * @return возвращает объект коннекшена
+	 */
+	private synchronized Connection openConnectionToBD() {
+		// собираю строку коннешена к БД
 		String connectionStr = String.
 				format("jdbc:mysql://%s:%s/%s?useSLL=false&serverTimezone=UTC", dbHost, dbPort, dbSchemaName);
-
-		// создаю коннешн и сохраняю его в поле объекта класса
-		dbConnection = DriverManager.getConnection(connectionStr, dbUser, dbPass);
-//		System.out.println("Connection successful!!!");
+		if (dbConnection == null) {
+			try {
+				dbConnection = DriverManager.getConnection(connectionStr, dbUser, dbPass);
+			} catch (SQLException e) {
+				System.out.println("Ошибка соединения с БД");
+			}
+		}
 		return dbConnection;
-	}
-
-	/**
-	 * метод для получения всех данных из БД в виде дерева и вывода на консоль
-	 */
-	public void getFullTree() {
-		// формат запроса на получение данных из БД
-		String query = "WITH RECURSIVE cte AS(\n" +
-				"SELECT id, parent_id, name, 0 lvl FROM folders\n" +
-				"WHERE parent_id IS NULL\n" +
-				"UNION ALL\n" +
-				"SELECT f.id, f.parent_id, CONCAT(REPEAT('  ',cte.lvl + 1), f.name), cte.lvl + 1 FROM cte\n" +
-				"JOIN folders AS f ON cte.id = f.parent_id)\n" +
-				"SELECT name FROM cte\n" +
-				"WHERE NOT name = 'ROOT'\n" +
-				"ORDER BY id, lvl";
-		queryHandler(query);
-	}
-
-	/**
-	 * метод для получения из БД отфильтрованных листьев
-	 *
-	 * @param str строка которая должна быть в именах
-	 */
-	public void getFilterLeafs(String str) {
-		// формат запроса на получение данных из БД
-		String query = String.format("SELECT f1.name\n" +
-				"FROM folders f1\n" +
-				"LEFT JOIN folders f2 ON f2.parent_id = f1.id\n" +
-				"WHERE f2.id IS NULL AND f1.name LIKE '%%%s%%';", str);
-		queryHandler(query);
-	}
-
-	/**
-	 * метод для получения из БД отфильтрованных данных в виде дерева и вывода на консоль
-	 * @param str строка которая должна быть в именах
-	 */
-	public void getFilterTree(String str) {
-		// формат запроса на получение данных из БД
-		String query = String.format("WITH RECURSIVE cte AS(\n" +
-				"SELECT id, parent_id, name, 0 lvl FROM folders\n" +
-				"WHERE parent_id IS NULL  \n" +
-				"UNION ALL\n" +
-				"SELECT f.id, f.parent_id, CONCAT(REPEAT('  ',cte.lvl + 1), f.name), cte.lvl + 1 FROM cte\n" +
-				"JOIN folders AS f ON cte.id = f.parent_id )\n" +
-				"SELECT id, parent_id, name, lvl FROM cte\n" +
-				"WHERE NOT name = 'ROOT' AND name LIKE '%%%s%%' \n" +
-				"ORDER BY id, lvl", str);
-		queryHandler(query);
 	}
 
 	/**
 	 * метод закрытия текущего коннекшена к БД
 	 */
-	public void closeConnectionToBD() {
+	public synchronized void closeConnectionToBD() {
 		if (dbConnection != null) {
 			try {
 				dbConnection.close();
@@ -97,6 +56,95 @@ public class DatabaseHandlerAdjacencyList {
 		}
 	}
 
+
+	/**
+	 * метод для получения всех данных из БД в виде дерева и вывод на консоль
+	 */
+	public void getFullTree() {
+		// формат запроса на получение данных из БД
+		String query = String.format("WITH RECURSIVE cte AS(\n" +
+				"SELECT id, parent_id, name, 0 lvl FROM %1$s\n" +
+				"WHERE parent_id IS NULL\n" +
+				"UNION ALL\n" +
+				"SELECT f.id, f.parent_id, CONCAT(REPEAT('  ',cte.lvl + 1), f.name), cte.lvl + 1 FROM cte\n" +
+				"JOIN %1$s AS f ON cte.id = f.parent_id)\n" +
+				"SELECT name FROM cte\n" +
+				"WHERE NOT name = 'ROOT'\n" +
+				"ORDER BY id, lvl", TABLE);
+		queryHandler(query);
+	}
+
+
+	/**
+	 * метод для получения из БД отфильтрованных данных в виде дерева и вывод на консоль
+	 *
+	 * @param str строка которая должна быть в именах конечных файлов
+	 */
+	public void getFilterTree(String str) {
+		createTmpTable();
+		clearTmpTable();
+		addToTmpTable(str);
+		getFilterTree();
+	}
+
+	/**
+	 * сервисный метод для создания временной таблицы в БД если она еще не создана
+	 */
+	private void createTmpTable() {
+		String query = String.format("CREATE TABLE IF NOT EXISTS %s (\n" +
+				"id INT(11) NOT NULL AUTO_INCREMENT,\n" +
+				"parent_id INT(11),\n" +
+				"name VARCHAR(200) NOT NULL,\n" +
+				"PRIMARY KEY (id))\n" +
+				"ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;", TMP_TABLE);
+		tableHandler(query);
+	}
+
+	/**
+	 * сервисный метод отчищения временной таблицы от данных
+	 */
+	private void clearTmpTable() {
+		// формат запроса на получение данных из БД
+		String query = String.format("TRUNCATE TABLE %s;", TMP_TABLE);
+		tableHandler(query);
+	}
+
+	/**
+	 * сервисный метод для который помещает во врменную таблицу отфильтрованные данные
+	 *
+	 * @param str подстрока, которая должна содержаться в конечных элементах дерева
+	 */
+	private void addToTmpTable(String str) {
+		String query = String.format("INSERT INTO %s\n" +
+				"WITH RECURSIVE cte AS (\n" +
+				"SELECT f1.id, f1.parent_id, f1.name AS leaf_list FROM folders f1\n" +
+				"LEFT JOIN folders f2 ON f2.parent_id = f1.id\n" +
+				"WHERE f2.id IS NULL AND f1.name LIKE '%%%s%%'\n" +
+				"UNION ALL\n" +
+				"SELECT f.id, f.parent_id, f.name FROM cte\n" +
+				"JOIN folders AS f ON cte.parent_id = f.id)\n" +
+				"SELECT DISTINCT * FROM cte\n" +
+				"ORDER BY id;", TMP_TABLE, str);
+		tableHandler(query);
+	}
+
+	/**
+	 * сервисный метод для вывода на консоль в виде дерева данных из временной таблицы
+	 */
+	private void getFilterTree() {
+		String query = String.format("WITH RECURSIVE cte AS(\n" +
+				"SELECT id, parent_id, name, 0 lvl FROM %1$s\n" +
+				"WHERE parent_id IS NULL\n" +
+				"UNION ALL\n" +
+				"SELECT f.id, f.parent_id, CONCAT(REPEAT('  ',cte.lvl + 1), f.name), cte.lvl + 1 FROM cte\n" +
+				"JOIN %1$s AS f ON cte.id = f.parent_id)\n" +
+				"SELECT name FROM cte\n" +
+				"WHERE NOT name = 'ROOT'\n" +
+				"ORDER BY id, lvl;", TMP_TABLE);
+		queryHandler(query);
+	}
+
+
 	/**
 	 * сервисный метод обрабатывающий запрос к БД, вывод на консоль и автоматическое закрытие запроса
 	 *
@@ -104,7 +152,7 @@ public class DatabaseHandlerAdjacencyList {
 	 */
 	private void queryHandler(String query) {
 		// создаю обращение к бд в try-with-resources
-		try (Statement st = getDbConnection().createStatement()) {
+		try (Statement st = openConnectionToBD().createStatement()) {
 			// выполняю запрос и получаю множесто результатов
 			ResultSet rs = st.executeQuery(query);
 			// итерируюсь по множеству
@@ -114,7 +162,17 @@ public class DatabaseHandlerAdjacencyList {
 				System.out.format("%s\n", name);
 			}
 		} catch (SQLException e) {
-			System.out.println("Проблема с доступом к БД");
+			System.out.println("Проблема с получением данных из БД");
+		}
+	}
+
+	private void tableHandler(String query) {
+		// создаю обращение к бд в try-with-resources
+		try (Statement st = openConnectionToBD().createStatement()) {
+			// выполняю запрос и получаю множество результатов
+			st.execute(query);
+		} catch (SQLException e) {
+			System.out.println("Проблема с созданием БД");
 		}
 	}
 }
